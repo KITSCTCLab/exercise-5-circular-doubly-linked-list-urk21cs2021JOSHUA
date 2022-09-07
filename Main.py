2b77134782b97ed0d56362945e9fedcaf179e6c4
urk21cs2021JOSHUA <109647940+urk21cs2021JOSHUA@users.noreply.github.com>
2022-09-07 10:26:54 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -116,3 +116,18 @@ for item in input_data.split(', '):
                 inner_list.append(int(letter))
         data.append(inner_list)
 obj = DoublyCircularLinkedList()
+result = []
+for i in range(len(operations)):
+    if operations[i] == "add_at_head":
+        result.append(obj.add_at_head(data[i]))
+    elif operations[i] == "add_at_tail":
+        result.append(obj.add_at_tail(data[i]))
+    elif operations[i] == "add_at_index":
+        result.append(obj.add_at_index(int(data[i][0]), data[i][1]))
+    elif operations[i] == "get":
+        result.append(obj.get(data[i]))
+    elif operations[i] == "get_previous_next":
+        result.append(obj.get_previous_next(data[i]))
+    elif operations[i] == 'delete_at_index':
+        result.append(obj.delete_at_index(data[i]))
+print(result)
